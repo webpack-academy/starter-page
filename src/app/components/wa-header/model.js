19390c5eeb9e0aeb3add134cd688030d20fbc1ae
Sean Larkin <sean.larkin@cuw.edu>
2016-10-01 23:23:00 -0500

    feat(style): rounded edges

--- src/app/components/wa-header/model.js
+++ src/app/components/wa-header/model.js
@@ -3,7 +3,7 @@ import styles from './style.css';
 export default function() {
 	const data = {
 		message: {
-			header: "Hello vuejs starter!!"		
+			header: "Hello webpack academy!!"		
 		},
 		styles
 	}
